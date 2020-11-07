71026e5a85894426b3ce350720384400ca3443f5
evan-carlsen <evan@crowdbotics.com>
2020-11-06 17:31:09 -0800

    Update screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,6 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-
+{name: 'SignupScreen114069', human_name: 'Signup Screen', access_route: 'SignupScreen114069'},
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ];
