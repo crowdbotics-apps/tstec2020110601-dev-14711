ac82b78daae563347826bc77df51e252c7cee650
evan-carlsen <evan@crowdbotics.com>
2020-11-09 12:45:28 -0800

    Update screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,8 +5,8 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
-import BlankScreen114195Navigator from '../features/BlankScreen114195/navigator';
-import BlankScreen314192Navigator from '../features/BlankScreen314192/navigator';
+//import BlankScreen114195Navigator from '../features/BlankScreen114195/navigator';
+//import BlankScreen314192Navigator from '../features/BlankScreen314192/navigator';
 import SignupScreen114069Navigator from "../features/SignupScreen114069/navigator";
 
 /**
@@ -16,15 +16,15 @@ import SignupScreen114069Navigator from "../features/SignupScreen114069/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
-BlankScreen114195: { screen: BlankScreen114195Navigator },
-BlankScreen314192: { screen: BlankScreen314192Navigator },
-    SignupScreen114069: {screen: SignupScreen114069Navigator},  
+//BlankScreen114195: { screen: BlankScreen114195Navigator },
+//BlankScreen314192: { screen: BlankScreen314192Navigator },
+ 
     /** new navigators can be added here */
     SplashScreen: {
       screen: SplashScreen
-    }
+    },
 
-
+    SignupScreen114069: {screen: SignupScreen114069Navigator}
 };
 
 const DrawerAppNavigator = createDrawerNavigator(
